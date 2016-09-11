20e022129f77377972a7962616331d4e0edc4c75
Akmuhammet Jumayev <akmuhammet@gmail.com>
2016-09-11 17:27:07 +0500

    Replace apostrophe with empty str

--- src/Behat/Transliterator/Transliterator.php
+++ src/Behat/Transliterator/Transliterator.php
@@ -573,8 +573,9 @@ abstract class Transliterator
             $text = strtolower($text);
         }
 
-        //AJU: replace apostrophe with empty string
+        //AJU: replace ' and . with empty string
         $text = str_replace("'", '', $text);
+        $text = str_replace(".", '', $text);
 
         // Remove all none word characters
         $text = preg_replace('/\W/', ' ', $text);
